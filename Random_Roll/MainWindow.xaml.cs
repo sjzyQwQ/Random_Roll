8e742e8e4992da41e92d94545f0fa89de0ce6176
Misaka10072 <misaka10072@outlook.com>
2024-10-01 16:47:03 +0800

    feat: 完成“关于”页

---
+++ Random_Roll/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using iNKORE.UI.WPF.Modern.Controls;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Page = iNKORE.UI.WPF.Modern.Controls.Page;
+
+namespace Random_Roll
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public Pages.About Page_About = new Pages.About();
+
+        private void NavigationView_SelectionChanged(NavigationView sender, NavigationViewSelectionChangedEventArgs args)
+        {
+            var item = sender.SelectedItem;
+            Page? page = null;
+
+            if (item == NavigationViewItem_Home)
+            {
+                //page = Page_Home;
+            }
+            else if (item == NavigationViewItem_Settings)
+            {
+                //page = Page_Settings;
+            }
+            else if (item == NavigationViewItem_About)
+            {
+                page = Page_About;
+            }
+
+            if (page != null)
+            {
+                NavigationView_Root.Header = page.Title;
+                Frame_Main.Navigate(page);
+            }
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            NavigationView_Root.SelectedItem = NavigationViewItem_Home;
+        }
+    }
+}
---
+++ Random_Roll/Pages/About.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Page = iNKORE.UI.WPF.Modern.Controls.Page;
+
+namespace Random_Roll.Pages
+{
+    /// <summary>
+    /// About.xaml 的交互逻辑
+    /// </summary>
+    public partial class About : Page
+    {
+        public About()
+        {
+            InitializeComponent();
+        }
+
+        private void Open_Repository_Click(object sender, RoutedEventArgs e)
+        {
+            Process.Start(new ProcessStartInfo { FileName = "https://github.com/sjzyQwQ/Random_Roll", UseShellExecute = true });
+        }
+    }
+}
